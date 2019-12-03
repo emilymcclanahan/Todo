8e1c04ce7122b96da541449d7da6446d3df3d3f6
Emily McClanahan <emilymcclanahan@wustl.edu>
2019-12-02 18:23:23 -0600

    Updates

---
+++ precache-manifest.4496f90a6ba8e9d8f9dbffff04b3d8a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "67ca029d1fb10d0a938a",
+    "url": "/09-todo-react-emilymcclanahan/static/css/main.6c1c1d1d.chunk.css"
+  },
+  {
+    "revision": "67ca029d1fb10d0a938a",
+    "url": "/09-todo-react-emilymcclanahan/static/js/main.67ca029d.chunk.js"
+  },
+  {
+    "revision": "9f640cdd66b112c280f1",
+    "url": "/09-todo-react-emilymcclanahan/static/js/1.9f640cdd.chunk.js"
+  },
+  {
+    "revision": "0f87e2b44674d8467983",
+    "url": "/09-todo-react-emilymcclanahan/static/js/runtime~main.0f87e2b4.js"
+  },
+  {
+    "revision": "fcb11b6e3edb01e42ce4cc45c32d0ea8",
+    "url": "/09-todo-react-emilymcclanahan/index.html"
+  }
+];
